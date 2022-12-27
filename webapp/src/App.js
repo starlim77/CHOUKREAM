4c931f081acf04eec3b37e779fd056e80b9222cd
hoilic <115633485+hoilic@users.noreply.github.com>
2022-12-27 10:15:27 +0900

    feat : searchForm 생성

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,14 +1,19 @@
+import React from 'react';
 import './App.css';
 import Layout from './component/Layout/Layout';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SearchForm from './component/Search/SearchForm';
+
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route path="/" element="" />
+                    <Route path="/" element="" />                    
                 </Route>
+                    <Route path="/Search/SearchForm" element={<SearchForm/>} />                    
             </Routes>
         </BrowserRouter>
     );
--- webapp/src/component/Header/Header.js
+++ webapp/src/component/Header/Header.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import * as S from './style';
 
+import SearchForm from '../Search/SearchForm';
+
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const onOpen = () => {
+        setIsOpen(true)
+    }
+    const onClose = () => {
+        setIsOpen(false)
+    }
     return (
         <>
             <S.HeaderWrapper>
@@ -22,7 +32,17 @@ const Header = () => {
                         <S.BottomLi>SHOP</S.BottomLi>
                         <S.BottomLi>ABOUT</S.BottomLi>
                         <S.BottomLi>
-                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            <div>
+                                <div onClick={onOpen}>
+                                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                                </div>
+                                {
+                                    isOpen && <SearchForm onClose={onClose}/>
+                                }
+                            </div>
+                        
+                            
+
                         </S.BottomLi>
                     </S.BottomWrapper>
                 </S.Bottom>
