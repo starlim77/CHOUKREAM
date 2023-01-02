fa5ff5c754bad221610797f55f314b2e4a6f21d3
starlim77 <starlim77@gmail.com>
2023-01-02 12:54:18 +0900

    .metadata 추가

--- workspace/FinalProject/src/main/java/com/main/FinalProjectApplication.java
+++ workspace/FinalProject/src/main/java/com/main/FinalProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan( basePackages = {"com.main","pay.*", "shop.*", "lookbook.*", "pay.*", "my.*"})
+@ComponentScan( basePackages = {"pay.*", "shop.*", "lookbook.*", "my.*"})
 @SpringBootApplication
 @EntityScan("*.bean")
 @EnableJpaRepositories("*.dao")
