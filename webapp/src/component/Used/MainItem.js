b63375d03b903c4f7a8609467b3b43842f74e024
jinkimjin <116698525+jinkimjin@users.noreply.github.com>
2023-01-06 14:01:11 +0900

    fix(Used) 메인 사진

--- webapp/src/component/Used/MainItem.js
+++ webapp/src/component/Used/MainItem.js
@@ -7,13 +7,26 @@ const MainItem = ({data,onItem}) => {
     // console.log(typeof(JSON.stringify(img)))
     
     const arr = JSON.stringify(data.imgName).split(',')
+    
+    //수정사항
+    // console.log(data.seq + ' :'  + typeof(arr) +  " arr : " + arr)
+
     const str = arr[0].slice(1)
+
+
+    if(!arr[1]){
+        // console.log(str.length)
+        // console.log(str)
+        var str2 = str.slice(0,str.length-1)
+        // console.log(str2)
+    }
+
     return (
 
         <>      
             <S.MainItem name={data.seq} onClick= {e => onItem(e.currentTarget.getAttribute("name"))}>
 
-                    <S.ItemImg src={`../storage/${str}`}/>
+                    <S.ItemImg src={`../storage/${str2 ? str2:str}`}/>
                     <S.ItemTitle>{data.title}</S.ItemTitle>
                     <S.ItemSubTitle>{data.productName}</S.ItemSubTitle>
                     <S.ItemContent>{data.contents}</S.ItemContent>
