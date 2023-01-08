3dba88dbeb1198bf431bb9ada9a4e54867eefeeb
HeonHong <116698529+HeonHong@users.noreply.github.com>
2023-01-09 04:37:22 +0900

    feat. usedItem 이미지 순서 바꾸기 기능

--- webapp/src/component/used/UsedItem.js
+++ webapp/src/component/used/UsedItem.js
@@ -54,7 +54,7 @@ const UsedItem = () => {
     const [subImg1,setSubImg1] = useState('')
     const [subImg2,setSubImg2] = useState('')
     const [subImg3,setSubImg3] = useState('')
-
+    const [allImg,setAllImg]=useState([]);
     useEffect(()=>{
 
         if((form.imgName)){
@@ -63,9 +63,11 @@ const UsedItem = () => {
 
             setMainImg(img[0])
         
-            img[1] && setSubImg1(img[1])
-            img[2] && setSubImg2(img[2])
-            img[3] && setSubImg3(img[3])
+            img[1] && setSubImg1(img[1]) 
+            img[2] && setSubImg2(img[2]) 
+            img[3] && setSubImg3(img[3]) 
+            setAllImg(img);
+
         }
     },[form])
 
@@ -94,6 +96,22 @@ const UsedItem = () => {
         
     }
 
+    //이미지 순서 바꾸기
+    const changImg=(e)=>{
+        var id = e.target.getAttribute("id");
+
+        var imgTemp=allImg[0];
+        allImg[0]=allImg[id];
+        allImg[id]=imgTemp;
+        
+        setMainImg(allImg[0]);
+        allImg[1] && setSubImg1(allImg[1]);
+        allImg[2] && setSubImg2(allImg[2]);
+        allImg[3] && setSubImg3(allImg[3]);
+        
+
+    }
+
     return (
 
         <>
@@ -102,10 +120,10 @@ const UsedItem = () => {
         </U.ModalDiv>
         <U.BaseBody>
             <U.ImgBody>
-                <U.MainImg src={`/storage/${mainImg}`}></U.MainImg>
-            {subImg1&&<U.SmallImg src={`/storage/${subImg1}`}></U.SmallImg>}
-            {subImg2&&<U.SmallImg src={`/storage/${subImg2}`}></U.SmallImg>}
-            {subImg3&&<U.SmallImg src={`/storage/${subImg3}`}></U.SmallImg>}
+                <U.MainImg src={`/storage/${mainImg}`} alt={mainImg}></U.MainImg>
+                {subImg1&&<U.SmallImg src={`/storage/${subImg1}`} id="1" onClick={e=>changImg(e)}></U.SmallImg>}
+                {subImg2&&<U.SmallImg src={`/storage/${subImg2}`} id="2" onClick={e=>changImg(e)}></U.SmallImg>}
+                {subImg3&&<U.SmallImg src={`/storage/${subImg3}`} id="3" onClick={e=>changImg(e)}></U.SmallImg>}
             </U.ImgBody>&emsp;
 
 
