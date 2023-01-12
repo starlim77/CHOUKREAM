6db526dd69db7fcebf3d2e71b2bf01018f707289
romano1994 <fhaksh0369@gmail.com>
2023-01-12 17:08:58 +0900

    feat(마이페이지): profile page 작업중

--- webapp/src/component/myPage/profile/Profile.js
+++ webapp/src/component/myPage/profile/Profile.js
@@ -6,6 +6,27 @@ const Profile = () => {
     return (
         <S.ProfileWrapper>
             <S.ProfileTitle>프로필 정보</S.ProfileTitle>
+            <S.Top>
+                <S.PicWrapper>
+                    <S.Picture src="../image/myProfile.png" />
+                </S.PicWrapper>
+
+                <S.MiddleWrapper>
+                    <S.IdDIv>id : asdasd</S.IdDIv>
+                    <S.ButtonWrapper>
+                        <S.Button>프로필 수정</S.Button>
+                        <S.Button>내 스타일</S.Button>
+                    </S.ButtonWrapper>
+                </S.MiddleWrapper>
+            </S.Top>
+            <S.Bottom>
+                <S.GroupTitle>로그인 정보</S.GroupTitle>
+                <S.Unit>
+                    <S.Title>이메일 주소</S.Title>
+                    <S.Email>a*******@gmail.com</S.Email>
+                    <S.ChangeButton>변경</S.ChangeButton>
+                </S.Unit>
+            </S.Bottom>
         </S.ProfileWrapper>
     );
 };
--- webapp/src/component/myPage/profile/ProfileStyle.js
+++ webapp/src/component/myPage/profile/ProfileStyle.js
@@ -16,3 +16,105 @@ padding-top: 10px;
 padding-bottom: 20px;
 border-bottom: 4px solid black;
 `
+export const Top = styled.div`
+display: flex;
+align-items: center;
+width: 100%;
+padding: 30px 10px;
+border-bottom: 1px solid #ebebeb;
+`
+export const PicWrapper = styled.div`
+display: flex;
+width: 15%;
+`
+export const Picture = styled.img`
+display: flex;
+width: 100%;
+border-radius: 70%;
+`
+export const MiddleWrapper = styled.div`
+display: flex;
+width: 60%;
+justify-content: space-between;
+flex-wrap: wrap;
+margin-left: 20px;
+`
+export const IdDIv = styled.div`
+display: flex;
+align-items: center;
+width: 50%;
+margin-right: 10%;
+margin-bottom: 20px;
+font-weight: 800;
+font-size: 22px;
+`
+export const EmailDIv = styled.div`
+display: flex;
+align-items: center;
+width: 50%;
+height: 33%;
+margin-right: 10%;
+margin-top: 2%;
+margin-bottom: 2%;
+color: rgba(34,34,34,.5);
+`
+export const ButtonWrapper = styled.div`
+display: flex;
+align-items: center;
+width: 50%;
+height: 33%;
+gap: 20px;
+margin-right: 10%;
+`
+export const Button = styled.button`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 130px;
+height: 40px;
+border: 1px solid #d3d3d3;
+border-radius: 10px;
+background-color: #fff;
+cursor: pointer;
+`
+export const Bottom = styled.div`
+display: flex;
+flex-wrap: wrap;
+width: 100%;
+`
+export const GroupTitle = styled.p`
+display: flex;
+width: 100%;
+margin: 40px 0;
+font-size: 18px;
+font-weight: 700;
+`
+export const Unit = styled.div`
+display: flex;
+flex-wrap: wrap;
+width: 60%;
+border-bottom: 1px solid #ebebeb;
+padding-bottom: 30px;
+`
+export const Title = styled.div`
+display: flex;
+width: 100%;
+margin-bottom: 20px;
+color: rgba(34,34,34,.5);
+`
+export const Email = styled.div`
+display: flex;
+width: 80%;
+color: rgba(34,34,34,.5);
+`
+export const ChangeButton = styled.button`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 60px;
+height: 35px;
+border: 1px solid #d3d3d3;
+border-radius: 10px;
+background-color: #fff;
+cursor: pointer;
+`
