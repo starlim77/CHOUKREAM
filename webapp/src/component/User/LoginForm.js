8f8a99e6eb6dde4f6c1fc2429c9e29dc99b91265
ykyk32 <111549119+ykyk32@users.noreply.github.com>
2023-01-18 14:05:55 +0900

    fix(스타일) 소셜 팔로잉버튼

--- webapp/src/component/User/LoginForm.js
+++ webapp/src/component/User/LoginForm.js
@@ -56,7 +56,7 @@ const LoginForm = () => {
             setRefreshToken(res.data.refreshToken)
             
             alert('로그인 성공')
-            navigate(-1)
+            navigate('/')
           })
           .catch(error => console.log(error))
     }
