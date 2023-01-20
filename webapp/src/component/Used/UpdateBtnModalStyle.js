fac874a46d2584c0a50d6f8eec2838bbb0b63623
starlim77 <starlim77@gmail.com>
2023-01-20 14:22:01 +0900

    Update UpdateBtnModalStyle.js

--- webapp/src/component/Used/UpdateBtnModalStyle.js
+++ webapp/src/component/Used/UpdateBtnModalStyle.js
@@ -1,36 +1,33 @@
-import styled from "styled-components";
+import { AddShoppingCartOutlined } from '@mui/icons-material';
+import styled from 'styled-components';
 
-export const ModalWrapper=styled.span`
-    width:100px;
-    height:100px;
+export const ModalWrapper = styled.span`
+    width: 100px;
+    height: 100px;
 `;
 
-export const MainBtnDiv=styled.div`
+export const MainBtnDiv = styled.div`
     height: 30px;
 `;
-export const UpdateBtn=styled.img`
-    padding:5px;
+export const UpdateBtn = styled.img`
+    padding: 5px;
     border-radius: 50%;
-    width:20px;
-    height:20px;
+    width: 20px;
+    height: 20px;
     float: right;
-    border:3px solid black;
+    border: 3px solid black;
     position: fixed;
-    top:20%;
+    top: 20%;
     right: 5%;
     background-color: white;
 `;
 
-export const SubBtnDiv=styled.div`
-
-`;
-export const BtnDiv=styled.div`
-    border:1px solid rgba(236,236,236);
-    height:60px;
+export const SubBtnDiv = styled.div``;
+export const BtnDiv = styled.div`
+    border: 1px solid rgba(236, 236, 236);
+    height: 60px;
     background: white;
     text-align: center;
     font-size: 25px;
-    line-height:60px;
-`
-
-
+    line-height: 60px;
+`;
